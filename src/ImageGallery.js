601b19e5bcf817590f396aaeaf05ce212d20c237
Ballha <148302546+Balhaa@users.noreply.github.com>
2024-01-11 08:06:20 +0700

    uas

--- src/ImageGallery.js
+++ src/ImageGallery.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+ import React, { useState, useEffect } from 'react';
 
 const images = [
   'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
